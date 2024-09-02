f2836464c4e2e46478bfd4da80705bac2b0a4d66
PriyansuGoWAla <126420530+PriyansuGoWAla@users.noreply.github.com>
2024-09-02 23:15:57 +0530

    app.js

--- app.js
+++ app.js
@@ -1,175 +1,43 @@
-// Mapping currency codes to country codes for flag images
 const countryList = {
-    AED: "AE",
-    AFN: "AF",
-    XCD: "AG",
-    ALL: "AL",
-    AMD: "AM",
-    ANG: "AN",
-    AOA: "AO",
-    ARS: "AR",
-    AUD: "AU",
-    AZN: "AZ",
-    BAM: "BA",
-    BBD: "BB",
-    BDT: "BD",
-    BGN: "BG",
-    BHD: "BH",
-    BIF: "BI",
-    BMD: "BM",
-    BND: "BN",
-    BOB: "BO",
-    BRL: "BR",
-    BSD: "BS",
-    BWP: "BW",
-    BYN: "BY",
-    BZD: "BZ",
-    CAD: "CA",
-    CDF: "CD",
-    CHF: "CH",
-    CLP: "CL",
-    CNY: "CN",
-    COP: "CO",
-    CRC: "CR",
-    CUP: "CU",
-    CVE: "CV",
-    CZK: "CZ",
-    DJF: "DJ",
-    DKK: "DK",
-    DOP: "DO",
-    DZD: "DZ",
-    EGP: "EG",
-    ERN: "ER",
-    ETB: "ET",
-    EUR: "EU",
-    FJD: "FJ",
-    FKP: "FK",
-    FOK: "FO",
-    GBP: "GB",
-    GEL: "GE",
-    GHS: "GH",
-    GIP: "GI",
-    GMD: "GM",
-    GNF: "GN",
-    GTQ: "GT",
-    GYD: "GY",
-    HKD: "HK",
-    HNL: "HN",
-    HRK: "HR",
-    HTG: "HT",
-    HUF: "HU",
-    IDR: "ID",
-    ILS: "IL",
-    INR: "IN",
-    IQD: "IQ",
-    IRR: "IR",
-    ISK: "IS",
-    JMD: "JM",
-    JOD: "JO",
-    JPY: "JP",
-    KES: "KE",
-    KGS: "KG",
-    KHR: "KH",
-    KMF: "KM",
-    KRW: "KR",
-    KWD: "KW",
-    KYD: "KY",
-    KZT: "KZ",
-    LAK: "LA",
-    LBP: "LB",
-    LKR: "LK",
-    LRD: "LR",
-    LSL: "LS",
-    LYD: "LY",
-    MAD: "MA",
-    MDL: "MD",
-    MGA: "MG",
-    MKD: "MK",
-    MMK: "MM",
-    MNT: "MN",
-    MOP: "MO",
-    MRU: "MR",
-    MUR: "MU",
-    MVR: "MV",
-    MWK: "MW",
-    MXN: "MX",
-    MYR: "MY",
-    MZN: "MZ",
-    NAD: "NA",
-    NGN: "NG",
-    NIO: "NI",
-    NOK: "NO",
-    NPR: "NP",
-    NZD: "NZ",
-    OMR: "OM",
-    PAB: "PA",
-    PEN: "PE",
-    PGK: "PG",
-    PHP: "PH",
-    PKR: "PK",
-    PLN: "PL",
-    PYG: "PY",
-    QAR: "QA",
-    RON: "RO",
-    RSD: "RS",
-    RUB: "RU",
-    RWF: "RW",
-    SAR: "SA",
-    SBD: "SB",
-    SCR: "SC",
-    SDG: "SD",
-    SEK: "SE",
-    SGD: "SG",
-    SHP: "SH",
-    SLL: "SL",
-    SOS: "SO",
-    SRD: "SR",
-    SSP: "SS",
-    STN: "ST",
-    SVC: "SV",
-    SYP: "SY",
-    SZL: "SZ",
-    THB: "TH",
-    TJS: "TJ",
-    TMT: "TM",
-    TND: "TN",
-    TOP: "TO",
-    TRY: "TR",
-    TTD: "TT",
-    TWD: "TW",
-    TZS: "TZ",
-    UAH: "UA",
-    UGX: "UG",
-    USD: "US",
-    UYU: "UY",
-    UZS: "UZ",
-    VES: "VE",
-    VND: "VN",
-    VUV: "VU",
-    WST: "WS",
-    XAF: "CF",
-    XCD: "AG",
-    XOF: "SN",
-    XPF: "PF",
-    YER: "YE",
-    ZAR: "ZA",
-    ZMW: "ZM",
+    AED: "AE", AFN: "AF", XCD: "AG", ALL: "AL", AMD: "AM", ANG: "AN", AOA: "AO", ARS: "AR", AUD: "AU",
+    AZN: "AZ", BAM: "BA", BBD: "BB", BDT: "BD", BGN: "BG", BHD: "BH", BIF: "BI", BMD: "BM", BND: "BN",
+    BOB: "BO", BRL: "BR", BSD: "BS", BWP: "BW", BYN: "BY", BZD: "BZ", CAD: "CA", CDF: "CD", CHF: "CH",
+    CLP: "CL", CNY: "CN", COP: "CO", CRC: "CR", CUP: "CU", CVE: "CV", CZK: "CZ", DJF: "DJ", DKK: "DK",
+    DOP: "DO", DZD: "DZ", EGP: "EG", ERN: "ER", ETB: "ET", EUR: "EU", FJD: "FJ", FKP: "FK", FOK: "FO",
+    GBP: "GB", GEL: "GE", GHS: "GH", GIP: "GI", GMD: "GM", GNF: "GN", GTQ: "GT", GYD: "GY", HKD: "HK",
+    HNL: "HN", HRK: "HR", HTG: "HT", HUF: "HU", IDR: "ID", ILS: "IL", INR: "IN", IQD: "IQ", IRR: "IR",
+    ISK: "IS", JMD: "JM", JOD: "JO", JPY: "JP", KES: "KE", KGS: "KG", KHR: "KH", KMF: "KM", KRW: "KR",
+    KWD: "KW", KYD: "KY", KZT: "KZ", LAK: "LA", LBP: "LB", LKR: "LK", LRD: "LR", LSL: "LS", LYD: "LY",
+    MAD: "MA", MDL: "MD", MGA: "MG", MKD: "MK", MMK: "MM", MNT: "MN", MOP: "MO", MRU: "MR", MUR: "MU",
+    MVR: "MV", MWK: "MW", MXN: "MX", MYR: "MY", MZN: "MZ", NAD: "NA", NGN: "NG", NIO: "NI", NOK: "NO",
+    NPR: "NP", NZD: "NZ", OMR: "OM", PAB: "PA", PEN: "PE", PGK: "PG", PHP: "PH", PKR: "PK", PLN: "PL",
+    PYG: "PY", QAR: "QA", RON: "RO", RSD: "RS", RUB: "RU", RWF: "RW", SAR: "SA", SBD: "SB", SCR: "SC",
+    SDG: "SD", SEK: "SE", SGD: "SG", SHP: "SH", SLL: "SL", SOS: "SO", SRD: "SR", SSP: "SS", STN: "ST",
+    SVC: "SV", SYP: "SY", SZL: "SZ", THB: "TH", TJS: "TJ", TMT: "TM", TND: "TN", TOP: "TO", TRY: "TR",
+    TTD: "TT", TWD: "TW", TZS: "TZ", UAH: "UA", UGX: "UG", USD: "US", UYU: "UY", UZS: "UZ", VES: "VE",
+    VND: "VN", VUV: "VU", WST: "WS", XAF: "CF", XOF: "SN", XPF: "PF", YER: "YE", ZAR: "ZA", ZMW: "ZM"
 };
 
 // Function to fetch exchange rates from the API
 async function getExchangeRates() {
     try {
         const response = await fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_vsAtetf35JMxiFk2yyLgv7KW3qoT4vOZgyZ733KB');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const data = await response.json();
         return data;
     } catch (error) {
         console.error('Error fetching exchange rates:', error);
+        alert('Failed to fetch exchange rates. Please try again later.');
     }
 }
 
 // Function to populate the dropdowns
 async function populateDropdowns() {
     const exchangeRatesData = await getExchangeRates();
+    if (!exchangeRatesData) return;
+
     const currencyOptions = Object.keys(exchangeRatesData.data);
     const fromCurrencySelect = document.getElementById('fromCurrency');
     const toCurrencySelect = document.getElementById('toCurrency');
@@ -191,7 +59,7 @@ async function populateDropdowns() {
 
     // Update flags initially
     updateFlags();
-}sz
+}
 
 // Function to update flags
 function updateFlags() {
@@ -208,6 +76,7 @@ async function getExchangeRate() {
     const toCurrency = document.getElementById('toCurrency').value;
     const amount = document.getElementById('amount').value;
     const exchangeRatesData = await getExchangeRates();
+    if (!exchangeRatesData) return;
 
     const fromRate = exchangeRatesData.data[fromCurrency].value;
     const toRate = exchangeRatesData.data[toCurrency].value;
